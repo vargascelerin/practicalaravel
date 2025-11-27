7d7ab60368e83645ded11d27997c5eef8da7f797
vargascelerin <vargascelerin@gmail.com>
2025-11-27 08:16:48 -0600

    Avances tests

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     */
+    public function test_that_true_is_true(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    public function test_status_code_is_200(): void
+    {
+        $statusCode = 200;
+        $this->assertEquals(200, $statusCode);
+    }
+}
