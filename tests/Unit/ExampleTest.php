ac1978ecf604ab453f98bfde4234bdd6a3a04a7a
vargascelerin <vargascelerin@gmail.com>
2025-12-01 08:11:02 -0600

    cambio assert true

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -11,7 +11,7 @@ class ExampleTest extends TestCase
      */
     public function test_that_true_is_true(): void
     {
-        $this->assertTrue(false);
+        $this->assertTrue(true);
     }
 
     public function test_status_code_is_200(): void
